12d52fdff00e53c58d479fb6b0a13f2cebfa11ef
Anton Klinker <klinker@live.dk>
2020-11-04 23:57:14 +0100

    made som random stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
 public class Controller {
+    public Button button;
+    public Label label;
+    public PasswordField password;
+    private String correctPassword = "ton";
+
+    public void buttonClicked(ActionEvent actionEvent) {
+
+        if (password.getText().equals(correctPassword)) {
+            label.setText("CORRECTOMUNDO");
+        } else {
+            label.setText("WRONG PASSWORD");
+        }
+
+    }
 }
